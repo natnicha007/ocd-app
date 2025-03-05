be62747890079c022988c96a46809dda84a78a43
“NatthaphatPiano” <Chaloei11111973@gmail.com>
2025-03-05 12:32:58 +0700

    first commit

---
+++ src/components/Portfolio2.tsx
@@ -0,0 +1,255 @@
+import React from "react";
+
+export default function Portolio2() {
+  return (
+    <div>
+      <>
+        <div className="full-title">
+          <div className="container">
+            {/* Page Heading/Breadcrumbs */}
+            <h1 className="mt-4 mb-3">
+              การจัดกิจกรรมเพื่อการพัฒนาองค์การภาครัฐ
+              <small>Subheading</small>
+            </h1>
+          </div>
+        </div>
+        {/* Page Content */}
+        <div className="container">
+          <div className="breadcrumb-main">
+            <ol className="breadcrumb">
+              <li className="breadcrumb-item">
+                <a href="index.html">หน้าหลัก</a>
+              </li>
+              <li className="breadcrumb-item active">
+                การจัดกิจกรรมเพื่อการพัฒนาองค์การภาครัฐ
+              </li>
+            </ol>
+          </div>
+          <div className="row">
+            <div className="col-lg-6 portfolio-item">
+              <div className="card h-100">
+                <a href="#">
+                  <img className="card-img-top" src="images/P11.jpg" alt="" />
+                </a>
+                <div className="card-body">
+                  <h4 className="card-title">
+                    <a
+                      href="assets/organizational intervention/organizational-intervention 1-41.pdf"
+                      target="_blank"
+                      rel="nonpener noreferrer"
+                    >
+                      การบริหารผลงาน
+                    </a>
+                  </h4>
+                  <p className="card-text">
+                    การบริหารผลงาน
+                    เป็นกลยุทธ์หนึ่งของการบริหารทรัพยากรบุคคลที่เน้นความสาคัญ
+                    ของการมีส่วนร่วมระหว่างผู้บังคับบัญชากับผู้ปฏิบัติงาน
+                    เพื่อผลักดันให้มีผลการปฏิบัติงานที่ดีขึ้น เป็นต้น
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 portfolio-item">
+              <div className="card h-100">
+                <a href="#">
+                  <img className="card-img-top" src="images/P33.jpg" alt="" />
+                </a>
+                <div className="card-body">
+                  <h4 className="card-title">
+                    <a
+                      href="assets/organizational intervention/organizational-intervention 42-75.pdf"
+                      target="_blank"
+                      rel="nonpener noreferrer"
+                    >
+                      การปรับโครงสร้างองค์การ
+                    </a>
+                  </h4>
+                  <p className="card-text">
+                    การปรับโครงสร้างองค์กร หมายถึง
+                    กระบวนการปรับโครงสร้างลำดับชั้น โครงสร้างภายใน
+                    หรือขั้นตอนการดำเนินงานของบริษัท บริษัทต่างๆ
+                    ดำเนินการปรับโครงสร้างเพื่อบรรลุเป้าหมายบางประการ เช่น
+                    เพื่อให้สามารถแข่งขันได้มากขึ้นหรือตอบสนองต่อการเปลี่ยนแปลงในตลาด
+                    เป็นต้น
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 portfolio-item">
+              <div className="card h-100">
+                <a href="#">
+                  <img className="card-img-top" src="images/P44.jpg" alt="" />
+                </a>
+                <div className="card-body">
+                  <h4 className="card-title">
+                    <a
+                      href="assets/organizational intervention/organizational-intervention 76-111.pdf"
+                      target="_blank"
+                      rel="nonpener noreferrer"
+                    >
+                      การมีส่วนร่วมของพนักงาน
+                    </a>
+                  </h4>
+                  <p className="card-text">
+                    การมีส่วนร่วมของพนักงานคือการได้รับรวมเป็นส่วนหนึ่งของทีมอย่างเต็มตัว
+                    มุ่งเน้นไปที่เป้าหมายที่ชัดเจน ได้รับความไว้วางใจและอำนาจ
+                    ได้รับการตอบรับเชิงบวกและสร้างสรรค์อย่างสม่ำเสมอ
+                    ได้รับการสนับสนุนในการพัฒนาทักษะใหม่ๆ
+                    ได้รับการขอบคุณและได้รับการยอมรับสำหรับความสำเร็จ เป็นต้น
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 portfolio-item">
+              <div className="card h-100">
+                <a href="#">
+                  <img className="card-img-top" src="images/P55.jpg" alt="" />
+                </a>
+                <div className="card-body">
+                  <h4 className="card-title">
+                    <a
+                      href="assets/organizational intervention/organizational-intervention 112-142.pdf"
+                      target="_blank"
+                      rel="nonpener noreferrer"
+                    >
+                      กลยุทธ์การแข่งขันและการมีส่วนร่วม
+                    </a>
+                  </h4>
+                  <p className="card-text">
+                    กลยุทธ์การมีส่วนร่วมของพนักงานเป็นกุญแจสำคัญในการสร้างทีมที่ให้ความสำคัญกับสิ่งอื่นๆ
+                    มากกว่าเงินเดือน เหตุใดจึงเป็นเช่นนั้น?
+                    เพราะพนักงานที่มีส่วนร่วมจะทุ่มเทอย่างเต็มที่กับบทบาทของตนเอง
+                    มีพลังจากงานที่ทำ
+                    และขับเคลื่อนด้วยความปรารถนาอย่างแท้จริงที่จะเห็นองค์กรของคุณเจริญรุ่งเรือง
+                    การใช้กลยุทธ์ที่ถูกต้องจะช่วยให้คุณสร้างพนักงานที่ไม่เพียงแต่มีแรงจูงใจแต่ยังมุ่งมั่นเพื่อความสำเร็จในระยะยาวของบริษัท
+                    พนักงานเหล่านี้ทุ่มเทอย่างเต็มที่เพื่อบรรลุภารกิจของคุณ
+                    ร่วมมือกับเพื่อนร่วมงาน
+                    และส่งเสริมสภาพแวดล้อมการทำงานที่ดีต่อสุขภาพ เป็นต้น
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 portfolio-item">
+              <div className="card h-100">
+                <a href="#">
+                  <img className="card-img-top" src="images/P56.webp" alt="" />
+                </a>
+                <div className="card-body">
+                  <h4 className="card-title">
+                    <a href="#">การออกแบบงาน</a>
+                  </h4>
+                  <p className="card-text">
+                    การออกแบบงาน
+                    เป็นเอกสารที่เขียนอย่างเป็นทางการซึ่งระบุความรับผิดชอบของงาน
+                    คุณสมบัติที่จำเป็น และหน้าที่
+                    โดยไม่นับรายละเอียดเกี่ยวกับค่าตอบแทนและสวัสดิการ
+                    เอกสารดังกล่าวจะจัดโครงสร้างและจัดระเบียบบทบาทงานต่างๆ
+                    ภายในองค์กรอย่างมีกลยุทธ์ ประกอบด้วยแนวทางต่างๆ
+                    ที่มุ่งเน้นที่การเพิ่มประสิทธิภาพ ความพึงพอใจของพนักงาน
+                    เป็นต้น
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 portfolio-item">
+              <div className="card h-100">
+                <a href="#">
+                  <img className="card-img-top" src="images/P89.jpg" alt="" />
+                </a>
+                <div className="card-body">
+                  <h4 className="card-title">
+                    <a href="#">การ Coaching</a>
+                  </h4>
+                  <p className="card-text">
+                    "การโค้ช (Coaching)"
+                    คือกระบวนการทำงานร่วมกันระหว่างโค้ชซึ่งเป็นเพื่อนชวนคิด
+                    หรือปลดล๊อคบางอย่างในตัวผู้รับการโค้ช (Coachee อ่านว่า
+                    โค้ชชี่) มีศักยภาพสูงขึ้น หรือมีความสุขอย่างที่เขาต้องการ
+                    ผ่านวิธีการและเครื่องมือต่างๆ เพื่อให้ผู้รับการโค้ช
+                    ได้เรียนรู้ ตระหนักในตัวเองและเปลี่ยนแปลง
+                    และลงมือทำด้วยความคิด ความถนัด เป็นต้น
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 portfolio-item">
+              <div className="card h-100">
+                <a href="#">
+                  <img className="card-img-top" src="images/P168.jpg" alt="" />
+                </a>
+                <div className="card-body">
+                  <h4 className="card-title">
+                    <a href="#">การสร้างทีม</a>
+                  </h4>
+                  <p className="card-text">
+                    การสร้างทีม การสร้างทีมงาน หมายถึง
+                    ความพยายามที่จะเข้าใจพฤติกรรมของคนที่ต้องมาปฏิบัติงานร่วมกัน
+                    และทำให้กลุ่มคนเหล่านั้นสามารถเรียนรู้ วิเคราะห์ปัญหา
+                    และหาทางออกในการปฏิบัติงานร่วมกัน
+                    เพื่อให้งานบรรลุเป้าหมายที่วางไว้ ดังนั้น
+                    หน่วยงานหรือองค์กรที่จะประสบความสำเร็จ
+                    ต้องมีการสร้างทีมที่มีความสามัคคี และมีเป้าหมายการทำงาน
+                    เป็นต้น
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 portfolio-item">
+              <div className="card h-100">
+                <a href="#">
+                  <img className="card-img-top" src="images/P369.webp" alt="" />
+                </a>
+                <div className="card-body">
+                  <h4 className="card-title">
+                    <a href="#">การเปลี่ยนวัฒนธรรมองค์กร</a>
+                  </h4>
+                  <p className="card-text">
+                    การเปลี่ยนแปลงวัฒนธรรมองค์กรคือกระบวนการพัฒนาวัฒนธรรมองค์กรของคุณในรูปแบบที่แท้จริงซึ่งดึงดูดผู้คนและสร้างแรงบันดาลใจให้ทีมทำงานได้อย่างเต็มประสิทธิภาพ
+                    เป็นเรื่องของการกำหนดและปลูกฝังพฤติกรรมที่ต้องการและปรับปรุงประสบการณ์ของพนักงานเพื่อเพิ่มการมีส่วนร่วม
+                    การรักษาพนักงาน ขวัญกำลังใจ
+                    และท้ายที่สุดช่วยให้คุณบรรลุวิสัยทัศน์ เป็นต้น
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* /.row */}
+          <div className="pagination_bar">
+            {/* Pagination */}
+            <ul className="pagination justify-content-center">
+              <li className="page-item">
+                <a className="page-link" href="#" aria-label="Previous">
+                  <span aria-hidden="true">«</span>
+                  <span className="sr-only">Previous</span>
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="#">
+                  1
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="#">
+                  2
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="#">
+                  3
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="#" aria-label="Next">
+                  <span aria-hidden="true">»</span>
+                  <span className="sr-only">Next</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        {/* /.container */}
+      </>
+    </div>
+  );
+}
